221242c92fc3299fdbe68d7aa0d65634877d556a
Rodzel John Te <rodzelte>
2024-02-13 20:13:26 +0800

    2nd commit

---
+++ landing.js
@@ -0,0 +1,6 @@
+document.getElementById('contactButton').addEventListener('click', function() {
+
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+    return(contactButton)
+  });
+
